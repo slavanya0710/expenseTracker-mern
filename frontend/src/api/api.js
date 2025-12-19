06b3d4e98723dfc274cc60f2e26c2aa5a8c1863b
lavanya <slavanya0710@gmail.com>
2025-12-19 14:24:30 +0530

    update api url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://expensetracker-aprb.onrender.com/api"
 });
 
 export const getTransactions = () => API.get("/transactions");
 export const addTransaction = (data) => API.post("/transactions", data);
-export const deleteTransaction = (id) =>
-  API.delete(`/transactions/${id}`);
-export const updateTransaction = (id, data) =>
-  API.put(`/transactions/${id}`, data);
+export const deleteTransaction = (id) => API.delete(`/transactions/${id}`);
+export const updateTransaction = (id, data) => API.put(`/transactions/${id}`, data);
